6a73b744bdb4706e95159a15bf60cccf89a3ed5b
ro.ershov <ro.ershov@tensor.ru>
2019-09-27 18:59:29 +0300

    some page

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const express = require('express');
-
+const
+   express = require('express'),
+   template = (src) => `
+      <!Doctype HTML>
+      <html>
+      <meta>
+         <meta charset="UTF-8"></meta>
+         <title> Юзаю вебпак </title>      
+      </head>
+      <body>
+         <script src="${src}"></script>
+      </body>
+      </html>
+   `;
+   
 app = express();
  
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/index.html');
+   res.send(template('./home.js'));
+});
+
+app.get('/about', (req, res) => {
+   res.send(template('./about.js'));
+});
+
+app.get('/contact', (req, res) => {
+   res.send(template('./contact.js'));
 });
    
 app.listen('8080', () => {console.log('express listening on port 8080')});
